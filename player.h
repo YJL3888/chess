b4985423b05c717758ef6c9a7c5c950f3cdaf3e4
Yuchen Jason Liu <52924882+YJL3888@users.noreply.github.com>
2024-07-22 00:28:58 -0400

    define player.h

--- player.h
+++ player.h
@@ -1,11 +1,21 @@
 #include <utility>
-#include "types.h"
+
 #include "board.h"
+#include "types.h"
 
 #ifndef PLAYER_H_
 #define PLAYER_H_
 
 class Player {
+ private:
+  bool isWhite;
+
+ public:
+  Player(bool isWhite);
+  virtual ~Player() {};
+  bool isCheckMate(Board* b, bool side);
+  bool isStaleMate(Board* b, bool side);
+  virtual std::pair<Position, Position> getMove(Board* b) = 0;
 };
 
 #endif
