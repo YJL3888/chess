5faaa6b0a559a2f0e71c727eb99d5513c5007ea7
Yuchen Jason Liu <52924882+YJL3888@users.noreply.github.com>
2024-07-21 23:40:46 -0400

    game.h definition

---
+++ game.h
@@ -0,0 +1,23 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <vector>
+
+#include "Board.h"
+#include "Player.h"
+class Game {
+ private:
+  Board board;
+  Player* p1;
+  Player* p2;
+  std::vector<Board*> moveHistory;
+  std::vector<Board*> redoHistory;
+
+ public:
+  Game();
+  void play();
+  void undo();
+  void redo();
+}
+
+#endif
