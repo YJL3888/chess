47dddd44cae320f94614b4c2f744149ac56ab2e0
Yuchen Jason Liu <52924882+YJL3888@users.noreply.github.com>
2024-07-22 23:56:07 -0400

    started human player get move

--- humanplayer.cc
+++ humanplayer.cc
@@ -6,7 +6,23 @@
 
 HumanPlayer::HumanPlayer(bool isWhite) : Player(isWhite) {};
 
-Move HumanPlayer::getMove(Board* b) {}
+Move HumanPlayer::getMove(Board* b) {
+  string first;
+  string second;
+  Move move;
+  while (cin >> first >> second) {
+    if (!(first.size() == 2 && second.size() == 2) &&
+        ('A' > toupper(first.at(0)) || toupper(first.at(0) > 'H')) &&
+        ('A' > toupper(second.at(0)) || toupper(second.at(0) > 'H'))) {
+      if (!cin) {
+        return move;
+      }
+      cout << "ERROR! Command should be in the format move a1 a2." << "\n";
+      cin >> first;
+      continue;
+    }
+  }
+}
 
 bool HumanPlayer::isValidMove(Board* b, Move move) const {
   std::vector<PotentialMoves> potentialMoves = b->allPotentialMoves(isWhite);
