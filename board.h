5acba1372e509430eeada8c991dcbf0157d84627
Yuchen Jason Liu <52924882+YJL3888@users.noreply.github.com>
2024-07-26 23:23:31 -0400

    added promote method to board h

--- board.h
+++ board.h
@@ -1,11 +1,12 @@
 #ifndef BOARD_H_
 #define BOARD_H_
-#include <vector>
 #include <iostream>
-#include "square.h"
-#include "types.h"
-#include "textdisplay.h"
+#include <vector>
+
 #include "gui.h"
+#include "square.h"
+#include "textdisplay.h"
+#include "types.h"
 using namespace std;
 
 class Board {
@@ -20,17 +21,20 @@ class Board {
   bool commandIntepreter(istream&, bool);
   Piece getPiece(Position);
   bool isFirstMove(Position);
-  Move previousMove(); // need this for en passant to verfiy the previous move of the opponent's pawn
+  Move previousMove();  // need this for en passant to verfiy the previous move
+                        // of the opponent's pawn
   int numMoves(Position);
 
-  vector<PotentialMoves> allPotentialMoves(bool); // returns all the potential moves of a player
+  vector<PotentialMoves> allPotentialMoves(
+      bool);  // returns all the potential moves of a player
 
-  bool inCheck(bool); // check if this colour is in check
-  void testMove(Move, bool=false); // bool is whether we perform this move or not, default is false
-  void reverseMove(Move, bool=false);
+  bool inCheck(bool);                 // check if this colour is in check
+  void testMove(Move, bool = false);  // bool is whether we perform this move or
+                                      // not, default is false
+  void reverseMove(Move, bool = false);
+  void promote(Position p, char promotedPiece);
 
   friend ostream& operator<<(ostream& out, const Board& b);
-
 };
 
 #endif
