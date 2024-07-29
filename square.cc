3703a17e9ef70ba6a020f65f04f6b4c0b17b112b
jennikaka <jennikakahui@gmail.com>
2024-07-29 00:42:42 -0400

    added reference for setState

--- square.cc
+++ square.cc
@@ -12,12 +12,12 @@ Square::Square(Position position, shared_ptr<ChessPiece> chessPiece, shared_ptr<
     notifyObservers();
 }
 
-void Square::setState(shared_ptr<ChessPiece> piece) {
+void Square::setState(shared_ptr<ChessPiece>& piece) {
     // chessPiece = piece;
     if(chessPiece){
         chessPiece->incrementMoves();
     }
-    chessPiece = piece;
+    chessPiece.swap(piece);
     notifyObservers();
 }
 
