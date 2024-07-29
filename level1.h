2c96f7107264ddeb89d6eaacd8ba0bc8aebacdeb
Yuchen Jason Liu <52924882+YJL3888@users.noreply.github.com>
2024-07-29 00:03:33 -0400

    merge conflict on level1 h file

--- level1.h
+++ level1.h
@@ -4,18 +4,10 @@
 #include "computerplayer.h"
 
 class Level1 : public ComputerPlayer {
-<<<<<<< HEAD
-  bool isWhite;
-
  public:
   Level1(bool isWhite);
-  std::pair<Position, Position> getMove(Board* board) override;
-=======
-public:
-    Level1(bool isWhite);
-    ~Level1() override;
-    Move getMove(Board* b) override;
->>>>>>> 8d44027f0fc8cc308f1927785cb701977606f4b0
+  ~Level1() override;
+  Move getMove(Board* b) override;
 };
 
 #endif
