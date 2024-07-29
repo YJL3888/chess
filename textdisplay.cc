db7a1d02866d48ef186bbf62a120956c652f08ac
jennikaka <jennikakahui@gmail.com>
2024-07-28 23:10:20 -0400

    fixed @ problem for board grid

--- textdisplay.cc
+++ textdisplay.cc
@@ -23,7 +23,7 @@ void TextDisplay::notify(Square& s) {
         case PieceType::King: character = 'K'; break;
         default: character = (posXY.first + posXY.second) % 2 == 1 ? ' ' : '_'; // show underline for a black/shaded square to create checker.
     }
-    if (s.getPiece().second == 0) {
+    if (s.getPiece().second == 0 && character <= 'Z' && character >= 'A') {
         character += 32; //convert to lower case letters for black player.
     }
     display[8-posXY.second][posXY.first-1] = character; //display[y][x] = char
